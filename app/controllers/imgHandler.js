71367fc94bf6aa578464ddfccb98b993848182bf
jammatpic <jammatpic@gmail.com>
2016-07-20 17:49:09 +1000

    Added formatted search results

--- app/controllers/imgHandler.js
+++ app/controllers/imgHandler.js
@@ -14,27 +14,7 @@ function imgHandler(db) {
 
     this.getResults = function(req, res) {
         var theUrl = "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + req.params.searchQuery;
-        var options = { url: theUrl }
-
-        function getAPIKey(callback) {
-            if (process.env.API_KEY) { // for heroku
-                options = {
-                    url: theUrl,
-                    headers: {
-                        "Ocp-Apim-Subscription-Key": process.env.API_KEY
-                    }
-                };
-            } else { // for running locally
-                fs.readFile("./config.json", "utf8", function (err, data) {
-                    // reading file, getting API key, and putting it into header
-                    options = {
-                        url: theUrl,
-                        headers: JSON.parse(data)
-                    };
-                });
-            }
-            callback();
-        };
+        var options = { url: theUrl };
 
         async.series([
             // getting API key before request
@@ -63,7 +43,18 @@ function imgHandler(db) {
             // getting search results from Bing Image Search API
             request(options, function(error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    res.end("It worked!");
+                    var results = JSON.parse(body);
+                    var resSummary = [];
+                    // building result sumamry for each result, and adding åit to array
+                    for (var i = 0; i < results.value.length; i++) {
+                        var resultSummary = {};
+                        resultSummary.url = results.value[i].contentUrl;
+                        resultSummary.snippet = results.value[i].name;
+                        resultSummary.thumbnail = results.value[i].thumbnailUrl;
+                        resultSummary.context = results.value[i].hostPageUrl;
+                        resSummary.push(resultSummary);
+                    }
+                    res.end(JSON.stringify(resSummary));
                 } else {
                     console.log(JSON.stringify(response));
                     res.end("API call error.")
@@ -72,8 +63,5 @@ function imgHandler(db) {
         });
     };
 };
-
-
-
 
 module.exports = imgHandler;
